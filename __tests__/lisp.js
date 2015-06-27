6a3ce726abe0dd2918f670daf756deb99c77fa33
Robin Ricard <ricard.robin@gmail.com>
2015-06-27 16:35:58 +0200

    Resolve uneeded

--- __tests__/lisp.js
+++ __tests__/lisp.js
@@ -5,7 +5,7 @@ var lisp = require("..");
 
 describe("lisp examples", () => {
   it("should translate simple function calls", () => {
-    expect(lisp([(a, b) => a + b, 1, 2]).resolve()).toBe(3);
-    expect(lisp([(a, b) => a + b, [(a, b) => a * b, 3, 3], 2]).resolve()).toBe(11);
+    expect(lisp([(a, b) => a + b, 1, 2])).toBe(3);
+    expect(lisp([(a, b) => a + b, [(a, b) => a * b, 3, 3], 2])).toBe(11);
   });
 });
--- index.js
+++ index.js
@@ -1,26 +1,15 @@
 "use strict";
 
-function resolve() {
-  var resolved = this.map(function resolveByCalling(something) {
-    if(typeof something.resolve === "function") {
-      return something.resolve();
-    }
-    return something;
-  });
-  if(typeof this[0] === "function") {
-    return resolved[0].apply(null, resolved.slice(1));
-  } else {
-    return resolved;
-  }
-}
-
 module.exports = function lispster(lisp) {
-  var resolvable = lisp.map(function makeArraysResolvableRecursively(something) {
+  var computed = lisp.map(function makeArraysResolvableRecursively(something) {
     if(something instanceof Array) {
       return lispster(something);
     }
     return something;
   });
-  resolvable.resolve = resolve.bind(resolvable);
-  return resolvable;
+  if(typeof computed[0] === "function") {
+    return computed[0].apply(null, computed.slice(1));
+  } else {
+    return computed;
+  }
 };
