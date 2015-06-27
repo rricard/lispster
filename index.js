3fd75e91a49102618a3818e0d2f8721d3c013ec0
Robin Ricard <ricard.robin@gmail.com>
2015-06-27 14:43:18 +0200

    First test (function calls) passing

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 "use strict";
 
+function resolve() {
+  var resolved = this.map(function resolveByCalling(something) {
+    if(typeof something.resolve === "function") {
+      return something.resolve();
+    }
+    return something;
+  });
+  if(typeof this[0] === "function") {
+    return resolved[0].apply(null, resolved.slice(1));
+  } else {
+    return resolved;
+  }
+}
+
 module.exports = function lispster(lisp) {
-  
+  var resolvable = lisp.map(function makeArraysResolvableRecursively(something) {
+    if(something instanceof Array) {
+      return lispster(something);
+    }
+    return something;
+  });
+  resolvable.resolve = resolve.bind(resolvable);
+  return resolvable;
 };
