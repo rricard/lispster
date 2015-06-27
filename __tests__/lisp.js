d8324bf60b60eff18ee1f196cd456a3449c9e043
Robin Ricard <ricard.robin@gmail.com>
2015-06-27 17:12:16 +0200

    Add lambda js macro

--- __tests__/lisp.js
+++ __tests__/lisp.js
@@ -1,11 +1,19 @@
 "use strict";
 
 jest.dontMock("..");
-var lisp = require("..");
+var lispster = require("..");
+var lisp = lispster.lisp;
+var lambda = lispster.lambda;
+var v = lispster.getVar;
 
 describe("lisp examples", () => {
   it("should translate simple function calls", () => {
     expect(lisp([(a, b) => a + b, 1, 2])).toBe(3);
     expect(lisp([(a, b) => a + b, [(a, b) => a * b, 3, 3], 2])).toBe(11);
+  });
+
+  it("should register lambdas", () => {
+    expect(lisp([lambda, ["x", "y"], [(a, b) => a + b, [v, "x"], [v, "y"]]])(1, 2)).toBe(3);
+    expect(lisp([[lambda, ["x", "y"], [(a, b) => a + b, [v, "x"], [v, "y"]]], 1, 2])).toBe(3);
   });
 });
--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 "use strict";
 
-module.exports = function lispster(lisp) {
+function lispster(lisp, scope) {
+  if(!scope) {
+    scope = {};
+  }
+  if(lisp[0] && lisp[0]._lispster_macro) {
+    return lisp[0]._lispster_macro.apply(scope, lisp.slice(1));
+  }
   var computed = lisp.map(function makeArraysResolvableRecursively(something) {
     if(something instanceof Array) {
-      return lispster(something);
+      return lispster(something, scope);
     }
     return something;
   });
   if(typeof computed[0] === "function") {
-    return computed[0].apply(null, computed.slice(1));
+    return computed[0].apply(scope, computed.slice(1));
   } else {
     return computed;
   }
+}
+exports.lisp = lispster;
+
+exports.lambda = {
+  _lispster_macro: function createLambda(argNames, block) {
+    var scope = {};
+    Object.keys(this).forEach(function copyScope(key) {
+      scope[key] = this[key];
+    }.bind(this));
+    return function lambda() {
+      var argIdx = 0;
+      var lambdaArguments = arguments;
+      argNames.forEach(function hashArgs(argName) {
+        scope[argName] = lambdaArguments[argIdx];
+        argIdx ++;
+      });
+      return lispster(block, scope);
+    };
+  }
+};
+
+exports.getVar = function getVar(varName) {
+  return this[varName];
 };
